6a57d29843e58155c39e5a36348f0ba51681fead
Marek <marek@pw.edu.pl>
2020-05-03 00:23:57 +0200

    Project TicTacToe - final version

--- Tic-Tac-Toe/Tic-Tac-Toe/task/src/tictactoe/Game.java
+++ Tic-Tac-Toe/Tic-Tac-Toe/task/src/tictactoe/Game.java
@@ -4,77 +4,89 @@ import java.util.Scanner;
 
 public class Game {
     private String[][] charOfCells = new String[3][3];
-    private String cells;
-    private int counter;
     private Scanner scanner = new Scanner(System.in);
+    private String turn = "X";
+    private boolean gameOver = false;
+    private String[] array = new String[2];
+    private String coordinates = "";
 
-    void play1() {
-        scanner = new Scanner(System.in);
-        System.out.print("Enter cells: ");
-        cells = scanner.nextLine();
-        readRows();
+    void play() {
+        start();
+        while (!gameOver) {
+            enterCoordinates();
+            changeTurn();
+            checkBoard();
+        }
+
+    }
+
+    private void start() {
+        for (int i = 0; i < charOfCells.length; i++) {
+            for (int j = 0; j < charOfCells[i].length; j++) {
+                charOfCells[i][j] = " ";
+            }
+        }
         drawBoard();
+    }
+
+    private void enterCoordinates() {
         System.out.print("Enter the coordinates: ");
-        String coordinates = scanner.nextLine();
-        String[] array = coordinates.split(" ");
-        while (!isNumeric(array[0]) || !isNumeric(array[1])) {
-            System.out.println("You should enter numbers!");
+        coordinates = scanner.nextLine();
+        array = coordinates.split(" ");
+        while (!checkValidate()) {
             System.out.print("Enter the coordinates: ");
             coordinates = scanner.nextLine();
             array = coordinates.split(" ");
         }
-        int row = Integer.parseInt(array[0]);
-        int column = Integer.parseInt(array[1]);
-        while (row > 3 || row < 1 || column > 3 || column < 1) {
-            System.out.println("Coordinates should be from 1 to 3!");
-            System.out.print("Enter the coordinates: ");
-            row = scanner.nextInt();
-            column = scanner.nextInt();
-        }
-        while (!charOfCells[row - 1][column - 1].equals("_")) {
-            System.out.println("This cell is occupied! Choose another one!");
-            System.out.print("Enter the coordinates: ");
-            row = scanner.nextInt();
-            column = scanner.nextInt();
-        }
-        charOfCells[row - 1][column - 1] = "X";
-        drawBoard();
     }
 
-    void play() {
-        scanner = new Scanner(System.in);
-        System.out.print("Enter cells: ");
-        cells = scanner.nextLine();
-        readRows();
+    private void changeTurn() {
+        int row = Integer.parseInt(array[0]);
+        int column = Integer.parseInt(array[1]);
+
+        if (turn.equals("X")) {
+            charOfCells[row - 1][column - 1] = "X";
+            turn = "O";
+        } else {
+            charOfCells[row - 1][column - 1] = "O";
+            turn = "X";
+        }
+    }
+
+    private boolean checkValidate() {
+        while (!isNumeric(array[0]) || !isNumeric(array[1])) {
+            System.out.println("You should enter numbers!");
+            return false;
+        }
+
+        while (Integer.parseInt(array[0]) > 3 || Integer.parseInt(array[0]) < 1 || Integer.parseInt(array[1]) > 3 || Integer.parseInt(array[1]) < 1) {
+            System.out.println("Coordinates should be from 1 to 3!");
+            return false;
+        }
+
+        while (!charOfCells[Integer.parseInt(array[0]) - 1][Integer.parseInt(array[1]) - 1].equals("_") && !charOfCells[Integer.parseInt(array[0]) - 1][Integer.parseInt(array[1]) - 1].equals(" ")) {
+            System.out.println("This cell is occupied! Choose another one!");
+            return false;
+        }
+        return true;
+    }
+
+    private void checkBoard() {
         drawBoard();
         if (xWins()) {
+            gameOver = true;
             System.out.println("X wins");
         } else if (oWins()) {
+            gameOver = true;
             System.out.println("O wins");
         } else if (draw()) {
+            gameOver = true;
             System.out.println("Draw");
-        } else if (gameNotFinished()) {
-            System.out.println("Game not finished");
-        } else if (impossible()) {
-            System.out.println("Impossible");
         }
     }
 
     private boolean isNumeric(String str) {
         return str.matches("-?\\d+(\\.\\d+)?");
-    }
-
-    private void readRows() {
-
-        for (int i = 0; i < 3; i++) {
-            charOfCells[i][2] = String.valueOf(cells.charAt(i));
-        }
-        for (int i = 0; i < 3; i++) {
-            charOfCells[i][1] = String.valueOf(cells.charAt(i + 3));
-        }
-        for (int i = 0; i < 3; i++) {
-            charOfCells[i][0] = String.valueOf(cells.charAt(i + 6));
-        }
     }
 
     private void drawBoard() {
@@ -83,29 +95,6 @@ public class Game {
         System.out.printf("| %s %s %s |%n", charOfCells[0][1], charOfCells[1][1], charOfCells[2][1]);
         System.out.printf("| %s %s %s |%n", charOfCells[0][0], charOfCells[1][0], charOfCells[2][0]);
         System.out.println("---------");
-    }
-
-    private boolean impossible() {
-        if (counter > 1) {
-            return true;
-        }
-        int numberOfX = 0;
-        int numberOfO = 0;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (charOfCells[i][j].equals("X")) {
-                    numberOfX++;
-                }
-                if (charOfCells[i][j].equals("O")) {
-                    numberOfO++;
-                }
-            }
-        }
-        if (Math.abs(numberOfO - numberOfX) > 1) {
-            return true;
-        }
-
-        return false;
     }
 
     private boolean xWins() {
@@ -130,21 +119,8 @@ public class Game {
         return draw;
     }
 
-    private boolean gameNotFinished() {
-        boolean notFinished = false;
-        for (int i = 0; i < charOfCells.length; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (charOfCells[i][j].equals("_") && !impossible()) {
-                    notFinished = true;
-                }
-            }
-        }
-        return notFinished;
-    }
-
     private String checkWinner() {
         String result = "";
-        counter = 0;
 
         for (int i = 0; i < 8; i++) {
             String line = "";
@@ -178,14 +154,10 @@ public class Game {
 
             if (line.equals("XXX")) {
                 result = "X";
-                counter++;
             } else if (line.equals("OOO")) {
                 result = "O";
-                counter++;
             }
-            if (counter > 1) {
-                result = "";
-            }
+
         }
         return result;
     }
