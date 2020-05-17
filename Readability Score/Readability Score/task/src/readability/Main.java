09ac7f3f6d21e127579477231853f7c152da3f02
Marek <marek@pw.edu.pl>
2020-05-17 15:18:23 +0200

    Project Readability Score 2/4

--- Readability Score/Readability Score/task/src/readability/Main.java
+++ Readability Score/Readability Score/task/src/readability/Main.java
@@ -7,10 +7,33 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
-        if (text.length() <=100) {
+        String[] stringsOfText = text.split("[.!?]+");
+        int sum = 0;
+        double average = 0;
+        String[] temp;
+        int empty = 0;
+
+        for (String item : stringsOfText) {
+            temp = item.trim().split("[^\\w,]");
+           //System.out.println("item: " + item);
+            for (String temp1 : temp) {
+                //System.out.println("temp1: " + temp1);
+                if (temp1.isEmpty()) {
+                    empty++;
+                }
+            }
+            sum += temp.length - empty;
+            //System.out.println("sum: " + sum);
+        }
+
+        average = (double) sum / stringsOfText.length;
+        //System.out.println("average: " + average);
+
+        if (average <= 10) {
             System.out.println("EASY");
         } else {
             System.out.println("HARD");
         }
+
     }
 }
