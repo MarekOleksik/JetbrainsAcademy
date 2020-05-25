dd5c3d001b93d517d7633a5f285f9d481c896079
Marek <marek@pw.edu.pl>
2020-05-25 14:25:50 +0200

    Project: Numeral System Converter stage 5/6

--- Numeral System Converter/Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/Numeral System Converter/task/src/converter/Main.java
@@ -8,26 +8,58 @@ public class Main {
         int sourceRadix = scanner.nextInt();
         String sourceNumber = scanner.next();
         int targetRadix = scanner.nextInt();
-        int decimalNumber = 0;
+        String[] splittedIntoIntegerAndFractional = new String[0];
+
+        int integerPartDecimal = 0;
+        double fractionalPartDecimal = 0;
+        String integerPartConverted = "";
 
         if (sourceRadix == 1) {
             String[] sourceNumbers = sourceNumber.split("");
             for (int i = 0; i < sourceNumbers.length; i++) {
-                decimalNumber += Integer.parseInt(sourceNumbers[i]);
+                integerPartDecimal += Integer.parseInt(sourceNumbers[i]);
             }
         } else {
-            decimalNumber = Integer.parseInt(sourceNumber, sourceRadix);
+            splittedIntoIntegerAndFractional = sourceNumber.split("\\.");
+            if (splittedIntoIntegerAndFractional.length <= 1) {
+                integerPartDecimal = Integer.parseInt(sourceNumber, sourceRadix);
+            } else {
+                integerPartDecimal = Integer.parseInt(splittedIntoIntegerAndFractional[0], sourceRadix);
+                String[] splittedFractionalPart = splittedIntoIntegerAndFractional[1].split("");
+                String[] splittedFractionalPartWithoutLetters = DigitsConverter.changeLettersToDigits(splittedFractionalPart);
+                for (int i = 1; i <= splittedIntoIntegerAndFractional[1].length(); i++) {
+                    fractionalPartDecimal += Double.parseDouble(splittedFractionalPartWithoutLetters[i - 1]) / Math.pow(sourceRadix, i);
+                }
+            }
         }
 
         if (targetRadix == 1) {
             String result = "";
-            for (int i = 0; i < decimalNumber; i++) {
+            for (int i = 0; i < integerPartDecimal; i++) {
                 result += "1";
             }
             System.out.println(result);
         } else {
-            System.out.println(Integer.toString(decimalNumber, targetRadix));
+            if (splittedIntoIntegerAndFractional.length <= 1) {
+                System.out.println(Integer.toString(integerPartDecimal, targetRadix));
+            } else {
+                integerPartConverted = Integer.toString(integerPartDecimal, targetRadix);
+                double temp = fractionalPartDecimal;
+                String result = "";
+                for (int i = 0; i < 5; i++) {
+                    double fractional = temp * targetRadix;
+                    String output = Double.toString(fractional);
+                    String[] outputs = output.split("\\.");
+                    result += DigitsConverter.changeDigitsToLetters(outputs[0]);
+                    temp = Double.parseDouble("0." + outputs[1]);
+                }
+
+                System.out.println(integerPartConverted + "." + result);
+            }
+
         }
 
     }
+
+
 }
