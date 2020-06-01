f02e52d61129cb1974504deda68df953a14dd3e0
Marek <marek@pw.edu.pl>
2020-06-01 21:58:07 +0200

    Project: Smart Calculator - stage 5/8

--- Smart Calculator/Smart Calculator/task/src/calculator/Main.java
+++ Smart Calculator/Smart Calculator/task/src/calculator/Main.java
@@ -1,6 +1,8 @@
 package calculator;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -11,42 +13,54 @@ public class Main {
         String input = "";
 
         while (!"/exit".equals(input)) {
-
-            input = scanner.nextLine();
-            if (input.isEmpty()) continue;
-            if (endProgram(input)) break;
-            if ("/help".equals(input)) {
-                System.out.println("The program calculates the sum of numbers");
-                System.out.println("The program support the addition +  and substraction - operators");
-                System.out.println("The even number of minuses gives a plus, and the odd number of minuses gives a minus!");
-                continue;
-            }
-            String inputReplaced = input.replaceAll("\\s+", " ").replaceAll("\\+\\+\\+", "+")
-                    .replaceAll("\\+\\+", "+").replaceAll("---", "-")
-                    .replaceAll("--", "+");
-
-            String[] items = inputReplaced.split("\\s+");
-            for (String item : items) {
-                //System.out.println("item: " + item);
-            }
-            int result = Integer.parseInt(items[0]);
-            if (items.length > 1) {
-                for (int i = 1; i < items.length; i++) {
-                    //System.out.println("items[i]: " + items[i]);
-                    //System.out.println("result: " + result);
-                    //System.out.println("items[i+1]: " + items[i+1]);
-                    switch (items[i]) {
-                        case "+":
-                            result = result + Integer.parseInt(items[i + 1]);
-                            break;
-                        case "-":
-                            result = result - Integer.parseInt(items[i + 1]);
-                            break;
-                    }
-                    i++;
+            try {
+                input = scanner.nextLine();
+                if (input.isEmpty()) continue;
+                if (endProgram(input)) break;
+                if ("/help".equals(input)) {
+                    System.out.println("The program calculates the sum of numbers");
+                    System.out.println("The program support the addition +  and substraction - operators");
+                    System.out.println("The even number of minuses gives a plus, and the odd number of minuses gives a minus!");
+                    continue;
                 }
+                String inputReplaced = input.replaceAll("\\s+", " ").replaceAll("\\+\\+\\+", "+")
+                        .replaceAll("\\+\\+", "+").replaceAll("---", "-")
+                        .replaceAll("--", "+");
+                Pattern pattern = Pattern.compile("\\/.*");
+                Matcher matcher = pattern.matcher(inputReplaced);
+                if (matcher.matches()) {
+                    System.out.println("Unknown command");
+                    continue;
+                }
+                String[] items = inputReplaced.split("\\s+");
+                for (String item : items) {
+                    //System.out.println("item: " + item);
+                }
+                String result = String.valueOf(Integer.parseInt(items[0]));
+                if (items.length > 1) {
+                    for (int i = 1; i < items.length; i++) {
+                        // System.out.println("items[i]: " + items[i]);
+                        //System.out.println("result: " + result);
+                        //System.out.println("items[i+1]: " + items[i+1]);
+                        switch (items[i]) {
+                            case "+":
+                                result = String.valueOf(Integer.parseInt(result) + Integer.parseInt(items[i + 1]));
+                                break;
+                            case "-":
+                                result = String.valueOf(Integer.parseInt(result) - Integer.parseInt(items[i + 1]));
+                                break;
+                            default:
+                                result = "Invalid expression";
+                                break;
+
+                        }
+                        i++;
+                    }
+                }
+                System.out.println(result);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid expression");
             }
-            System.out.println(result);
         }
     }
 
